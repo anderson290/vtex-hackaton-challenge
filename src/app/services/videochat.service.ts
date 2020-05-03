7c0a6a2c1b62151d9cd3bc313e17ad0e659c25e8
unknown <anderson.nunes17@etec.sp.gov.br>
2020-05-03 19:50:00 -0300

    Inserindo barra ajuda video conf

--- src/app/services/videochat.service.ts
+++ src/app/services/videochat.service.ts
@@ -1,9 +1,83 @@
+import { connect, ConnectOptions, LocalTrack, Room } from 'twilio-video';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ReplaySubject , Observable } from 'rxjs';
+
+interface AuthToken {
+    token: string;
+    room: string;
+}
+
+export interface NamedRoom {
+    id: string;
+    name: string;
+    maxParticipants?: number;
+    participantCount: number;
+}
+
+export type Rooms = NamedRoom[];
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class VideochatService {
+export class VideoChatService {
+    $roomsUpdated: Observable<boolean>;
 
-  constructor() { }
+    private roomBroadcast = new ReplaySubject<boolean>();
+
+    constructor(private readonly http: HttpClient) {
+        this.$roomsUpdated = this.roomBroadcast.asObservable();
+    }
+
+    private async getAuthToken() {
+        const auth =
+            await this.http
+                      .get<AuthToken>(`http://localhost:3000/twilio/token`)
+                      .toPromise();
+        return auth.token;
+    }
+
+    async getRoomName() {
+        const auth =
+            await this.http
+                      .get<AuthToken>(`http://localhost:3000/twilio/token`)
+                      .toPromise();
+                      console.log(auth)
+        return auth.room;
+    }
+
+
+    getAllRooms() {
+
+        return this.http
+                   .get<Rooms>('http://localhost:3000/twilio/rooms/DailyStandup')
+                   .toPromise();
+    }
+
+    async joinOrCreateRoom(name: string, tracks: LocalTrack[]) {
+        let room: Room = null;
+        try {
+            const token = await this.getAuthToken();            
+            room =
+                await connect(
+                    token, {
+                        name,
+                        tracks,
+                        dominantSpeaker: true
+                    } as ConnectOptions);
+                    console.log(room);
+        } catch (error) {
+            console.error(`Unable to connect to Room: ${error.message}`);
+        } finally {
+            if (room) {
+                this.roomBroadcast.next(true);
+            }
+        }
+
+        return room;
+    }
+
+    nudge() {
+        this.roomBroadcast.next(true);
+    }
 }
--- src/app/video/rooms/rooms.component.ts
+++ src/app/video/rooms/rooms.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter, Output, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { NamedRoom, VideoChatService } from './../../services/videochat.service';
 
 @Component({
-  selector: 'app-rooms',
-  templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+    selector: 'app-rooms',
+    styleUrls: ['./rooms.component.scss'],
+    templateUrl: './rooms.component.html',
 })
-export class RoomsComponent implements OnInit {
+export class RoomsComponent implements OnInit, OnDestroy {
+    @Output() roomChanged = new EventEmitter<string>();
+    @Input() activeRoomName: string;
 
-  constructor() { }
+    roomName: string;
+    rooms: NamedRoom[];
 
-  ngOnInit(): void {
-  }
+    private subscription: Subscription;
 
+    constructor(
+        private readonly videoChatService: VideoChatService) { }
+
+    async ngOnInit() {
+        await this.updateRooms();
+        this.subscription =
+            this.videoChatService
+                .$roomsUpdated
+                .pipe(tap(_ => this.updateRooms()))
+                .subscribe();
+
+        const name = await this.videoChatService.getRoomName();
+
+        this.roomName = name;
+
+        this.onJoinRoom(this.roomName);
+        
+    }
+
+    ngOnDestroy() {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
+    onTryAddRoom() {
+        if (this.roomName) {
+            this.onAddRoom(this.roomName);
+        }
+    }
+
+    onAddRoom(roomName: string) {
+        this.roomName = null;
+        this.roomChanged.emit(roomName);
+    }
+
+    onJoinRoom(roomName: string) {
+        this.roomChanged.emit(roomName);
+    }
+
+    async updateRooms() {
+        this.rooms = (await this.videoChatService.getAllRooms()) as NamedRoom[];
+    }
 }
