f35e6dd155e2fe42672f645073b8696900464cef
Marcos Souza <marcos.souza@fcamara.com.br>
2020-05-03 23:20:58 -0300

    Adjusted medicines images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,28 +19,32 @@ export class HomeComponent implements OnInit {
         title: "Dipirona",
         description: "Combate a dor de cabeça!",
         price: 10.00,
-        company_id: 2
+        company_id: 2,
+        image: "https://img.drogasil.com.br/catalog/product/m/e/medley_dipirona_500mg.jpg?width=520&height=520&quality=50&type=resize"
       },
       {
         _id: 2,
         title: "Paracetamol",
-        description: "Combate a dor de cabeça e corpo!",
+        description: "Combate a dor de cabeça!",
         price: 12.00,
-        company_id: 3
+        company_id: 3,
+        image: "https://img.onofre.com.br/catalog/product/p/a/paracetamol_200mgml_solu_o_cimed.jpg?width=520&height=520&quality=50&type=resize"
       },
       {
         _id: 22,
         title: "Alênia",
         description: "Respiração",
         price: 21.40,
-        company_id: 12
+        company_id: 12,
+        image: "https://img.drogasil.com.br/catalog/product/a/r/artrogen_duo_01.jpg?width=520&height=520&quality=50&type=resize"
       },
       {
         _id: 34,
         title: "Dorflex",
         description: "Combate dores no corpo!",
         price: 8.40,
-        company_id: 10
+        company_id: 10,
+        image: "https://img.drogasil.com.br/catalog/product/d/o/dorflex-com-36-comprimidos-7891058017507-_2_.jpg?width=520&height=520&quality=50&type=resize"
       }
     ]
   }
